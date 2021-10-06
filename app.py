3639303567992a216e3c42fa2ccce62010c9f8a8
Wicharn Rueangkhajorn <wicharn.keisei@gmail.com>
2021-10-06 21:50:14 +0700

    feat: add health check for sub model

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import dotenv_values
 from flask import Flask, jsonify, request
-
+import requests
 
 is_production = os.environ.get('FLASK_ENV') == 'production'
 authentication_token = os.environ.get('AUTHORIZATION_KEY')
@@ -13,4 +13,9 @@ app = Flask(__name__)
 @app.route('/healthcheck')
 def health_check():
     return 'OK'
+
+@app.route('/healthcheck/model')
+def health_check_model():
+    response = requests.get("http://127.0.0.1:80/api/model/summarizer/healthcheck")
+    return response
 
