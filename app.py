a6e23f593618f350f22477e29121d44a0a3a79bb
Wicharn Rueangkhajorn <wicharn.keisei@gmail.com>
2021-10-29 17:22:43 +0700

    feat: end point for predict unstructured text input

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 from Client import Client
+from utils.FileParser import FileParser
 
 is_production = os.environ.get('FLASK_ENV') == 'production'
 authentication_token = os.environ.get('AUTHORIZATION_KEY')
@@ -17,6 +18,8 @@ CORS(app)
 
 caches = {}
 CACHE_TIMEOUT_IN_MINUTE = 30
+
+file_parser = FileParser()
 
 @app.route('/healthcheck')
 def health_check():
@@ -49,18 +52,40 @@ def semi_structured_predict():
                 return jsonify({"error": "Data format wrong"}), 400
     return jsonify({"error": "Data is invalid or not exist"}), 400
 
+@app.route('/predict/unstructured/text', methods=['POST'])
+def unstructured_text_predict():
+    if request.method == 'POST' and request.json is not None:
+        if is_production:
+            token = request.headers.get('Authorization').split(' ')[1]
+            if token != authentication_token:
+                return jsonify({"error": "Unauthorized request"}), 403
+        topic = request.json['topic']
+        text = request.json['text']
+        if topic is not None and text is not None:
+            try:
+                result = Client.generate_mind_map_from_unstructured_text(
+                    topic, text)
+                return jsonify({"result": result})
+            except Exception as e:
+                print(topic, flush=True)
+                print(text, flush=True)
+                print(e, flush=True)
+                return jsonify({"error": "Data format wrong"}), 400
+    return jsonify({"error": "Data is invalid or not exist"}), 400
 
-@app.route('/predict/unstructured', methods=['POST'])
-def unstructured_predict():
+@app.route('/predict/unstructured/file', methods=['POST'])
+def unstructured_file_predict():
     if request.method == 'POST':
         if is_production:
             token = request.headers.get('Authorization').split(' ')[1]
             if token != authentication_token:
                 return jsonify({"error": "Unauthorized request"}), 403
         topic = request.form['topic']
-        text = request.files['text']
-        if text.filename == '':
-            text = ''
+        input_file = request.files['file']
+        if input_file.filename == '':
+            return jsonify({"error": "file not found"}), 400
+        else:
+            text = file_parser.parse_file(input_file)
         if topic is not None and text is not None:
             try:
                 result = Client.generate_mind_map_from_unstructured_text(
